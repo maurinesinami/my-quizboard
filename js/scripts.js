7f53e5fac552f708cb6378c8419a02ddcc8bce33
maurinenami <maurinenami>
2019-03-31 17:01:36 +0300

    add the whole page

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,21 @@ function set (){
 $(document).ready(function(){
     $("#quiz").submit(function(event){
         event.preventDefault();
-        var a=parseInt)($("input:radio[name=Q1]:checked").val());
+        var a=parseInt($("input:radio[name=Q1]:checked").val());
+        var b=parseInt($("input:radio[name=Q2]:checked").val());
+        var c=parseInt($("input:radio[name=Q3]:checked").val());
+        var d=parseInt($("input:radio[name=Q4]:checked").val());
+        var e=parseInt($("input:radio[name=Q5]:checked").val());
+
+        var total=parseInt((a+b+c+d+e) /50*100);
+        var name=$("input#firstName").val();
+        var name=$("input#secondName").val();
+        $("#results").text(name +"your score is:"+"display"+"%");
+        $("#seeResults").click(function(){
+            $("#begin").hide();
+            $("#questions").hide();
+            $("#answers").show();
+          });
+
     })
 })
