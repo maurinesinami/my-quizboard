09053f9f12611a6cccc5ee2bb1176c48435906b0
maurinenami <maurinenami>
2019-04-01 09:45:25 +0300

    creates output for results

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,57 @@
 //Business logic //
-function set() {
-    var name = document.getElementById("firstName").Value;
-    var name = document.getElementById("secondName").value;
-}
+$(document).ready(function () {
+  $("#btn1").click(function() {
+    var nameOne = $("input#name1").val();
+    var nameTwo = $("input#name2").val();
+
+  })
+})
+    
+    
+
 
 //U.I Logic//
 $(document).ready(function () {
     $("#quiz").submit(function (event) {
         event.preventDefault();
-        var a = parseInt($("input:radio[name=Q1]:checked").val());
-        var b = parseInt($("input:radio[name=Q2]:checked").val());
-        var c = parseInt($("input:radio[name=Q3]:checked").val());
-        var d = parseInt($("input:radio[name=Q4]:checked").val());
-        var e = parseInt($("input:radio[name=Q5]:checked").val());
+        var quiz1= $("input:radio[name=Q1]:checked").val();
+        var quiz2 = $("input:radio[name=Q2]:checked").val();
+        var quiz3= $("input:radio[name=Q3]:checked").val();
+        var quiz4 = $("input:radio[name=Q4]:checked").val();
+        var quiz5= $("input:radio[name=Q5]:checked").val();
 
-        var total = parseInt((a + b + c + d + e) / 50 * 100);
-        var name = $("input#firstName").val();
-        var name = $("input#secondName").val();
-        if (percent > 79) {
-            $("#correct").text( " You scored " + percent + "%. " + "EXCELLENT");
-          } else if (percent > 59 && percent < 79) {
-            $("#correct").text(" You scored " + percent + "%. " + "FAIR YOU CAN DO BETTER");
-          } else if (percent > 39 && percent < 59) {
-            $("#correct").text(" You scored " + percent + "%. " + "Below Average !.");
-          } else if(percent >0 && percent<20) {
-            $("#correct").text(" You scored " + percent + "%. " + "POOR!");
-          }else if(percent === 0){
-            $("#correct").text("You scored "+ percent+ " %. " + " !!!!! ");
-          } else{
-            $("#correct").text("You need to answer ALL the questions")
-          };
-            $("#hidden").fadeToggle();
-          });
-        });
-      });
       
-        $("#submit").click(function () {
-            $("#results").slideToggle();
-        });
+        
+        var correct = 0;
+        if (quiz1=="Style"){
+          correct++;
+        }
+        if (quiz2=="HTML 5"){
+          correct++;
+        }
+        if (quiz3=="Interpreted"){
+          correct++;
+        }
+        if (quiz4=="Deceleration"){
+          correct++;
+        }
+        if (quiz5=="Browser"){
+          correct++;
+        }
+        var percent = correct/5*100
+        if (percent >79){
+          remark="EXELLENT!"
+        }
+        if(percent >49){
+          remark="FAIR!"
+
+        }
+        else {
+          remark="!!!!"
+        };
+ 
+      document.getElementById("correct").innerHTML = "You got " + correct +" out of 5 questions correct." 
+    document .getElementById("results").innerHTML = remark;
     });
 
 });
